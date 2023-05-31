f501fc7f186fa306e4623face2f63be75818256e
Orange4Broom <orang4broom@gmail.com>
2023-05-31 09:19:08 +0200

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import './index.scss';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Favourites from './pages/favourites/Favourites';
+import Cart from './pages/cart/Cart';
+import Login from './pages/login/Login';
+import Product from './pages/product/Product';
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/favourites" element={<Favourites />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/product/:id" element={<Product />} />
+    </Routes>
   );
 }
 
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,18 @@
+import './navigation.scss';
+
+function Navigation() {
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">
+          <img src="#" alt="logo" />
+        </a>
+        <ul className="nav-list">
+          <li></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
